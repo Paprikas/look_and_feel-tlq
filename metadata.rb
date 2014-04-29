62d7ff8249a06f02b9f94ffbc0a8ff5705e0f02d
Max Paprikas <kombise@gmail.com>
2014-04-30 03:15:32 +0600

    Fix naming

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "look_and_feel-tlq"
+name              "look-and-feel-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@hillsbede.co.uk"
 description       "Installs tools to make the server nicer to work on, quite opinionated"
-version           "0.0.1"
+version           "0.0.2"
 
-recipe "look_and_feel-tlq", "Installs all look and feel options"
+recipe "look-and-feel-tlq", "Installs all look and feel options"
 
 supports "ubuntu"
